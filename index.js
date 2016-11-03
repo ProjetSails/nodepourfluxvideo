7249e10aba4ba592145dcef2a80db085e86497ad
Baptiste <baptiste.lanusse@ynov.Com>
2016-11-03 21:12:35 +0100

    test port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if( process.argv.length < 3 ) {
 }
 
 var STREAM_SECRET = process.argv[2],
-	STREAM_PORT = process.argv[3] || 8082,
+	STREAM_PORT = process.argv[3] || 80,
 	WEBSOCKET_PORT = process.argv[4] || 8084,
 	STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes
 
